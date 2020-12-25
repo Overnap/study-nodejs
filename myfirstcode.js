65c87f79e5c306824f36942078eb7edc4d1184f8
Overnap <overnapworks@gmail.com>
2020-12-25 19:16:39 +0900

    try to use express

--- myfirstcode.js
+++ myfirstcode.js
@@ -1,37 +1,11 @@
-import { createServer } from 'http';
-import { readFile } from 'fs';
+const express = require('express');
+const app = express();
+const PORT = 3001;
 
-// Simple router?
-class App {
-    constructor() {
-        this.map = new Map();
-        this.server = createServer((req, res) => {
-            var reqData = new URL(req.url, `http://${req.headers.host}`);
-            console.log(`${reqData.host} : ${reqData.pathname}`);
-            if (this.map.has(reqData.pathname))
-                this.map.get(reqData.pathname)(res, reqData.searchParams);
-        })
-    }
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-    run(port, callback) {
-        this.server.listen(port, callback);
-    }
-
-    route(path, callback) {
-        this.map.set(path, callback);
-    }
-}
-
-const PORT = process.env.PORT || 3000;
-
-const app = new App();
-app.route('/hello', (res, params) => { res.write('hello world'); res.end(); })
-app.route('/htmltest', (res, params) => {
-    readFile('./index.html', 'utf-8', (err, data) => {
-        if (err) throw err; html = data;
-        res.setHeader("Content-Type", "text/html");
-        res.write(html);
-        res.end();
-    })
-})
-app.run(PORT, () => { console.log(`${PORT} 듣는중...`); })
+app.listen(PORT, () => {
+    console.log(`example server... http://localhost:${PORT}`);
+});
